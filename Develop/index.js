fe7cf07eb92de14a4cf6f5c27916df43e5f04207
Britain Gilgour <britaing@gmail.com>
2023-08-14 13:23:14 -0500

    cleaned up code

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,12 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-// TODO: Create an array of questions for user input
 const questions = () => inquirer.prompt([
     {
         name: 'Title',
         type: 'input',
         message: 'what is the title of your project?'
     },
-
     {
         name: 'Description',
         type: 'input',
@@ -32,52 +28,31 @@ const questions = () => inquirer.prompt([
         type: 'input',
         message: "Who are the contributing programmers to your project?"
     },
-
     {
         name: 'Contribute',
         type: 'input',
         message: "How can other coders contribute to this project?"
     },
-
     {
         name: 'Tests',
         type: 'input',
         message: 'Do you have examples of how to run tests with your program?'
     }
-
 ]);
 
 // TODO: Create a function to write README file
 questions().then((answers) => {
     const generated = generateMarkdown(answers);
     fs.writeFileSync('README.md', generated)
-    }).then(() => console.log('Your README file has been written!');)
-
-
+    }).then(() => console.log('Your README file has been written!'))
 
 // TODO: Create a function to initialize app
 function init() {
 
 }
-
 // Function call to initialize app
 init();
 
 
 
 
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
